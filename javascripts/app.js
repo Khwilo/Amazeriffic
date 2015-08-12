c82064c129260208c6b448aae4f11b037a5732db
Khwilo <kwhyzzy@gmail.com>
2015-08-12 08:38:48 +0300

    set up the second tab

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,11 +1,20 @@
 var main = function () {
   "use strict";
 
+  var toDos = [
+    "Learn Ruby on Rails",
+    "Go to JKUAT",
+    "Buy soccer boots",
+    "Visit friends",
+    "Practice for the church concert",
+    "Head for a road trip"
+  ];
+
   $(".tabs a span").toArray().forEach(function (element){
     // create a click handler for the event
     $(element).on("click", function (){
       // create a temporary jQuery version variable
-      var $element = $(element);
+      var $element = $(element), $content;
 
       $(".tabs a span").removeClass("active");
       $element.addClass("active");
@@ -14,7 +23,11 @@ var main = function () {
       if ($element.parent().is(":nth-child(1)")) {
         console.log("FIRST TAB IS CLICKED!");
       }else if ($element.parent().is(":nth-child(2)")) {
-        console.log("SECOND TAB IS CLICKED");
+        $content = $("<ul>");
+        toDos.forEach(function (todo){
+          $content.append($("<li>").text(todo));
+        });
+        $("main .content").append($content);
       }else if ($element.parent().is(":nth-child(3)")) {
         console.log("THIRD TAB IS CLICKED");
       }
