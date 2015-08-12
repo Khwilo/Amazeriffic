8c701d979a70e041082a099587a394fdfadb68c1
Khwilo <kwhyzzy@gmail.com>
2015-08-12 08:28:03 +0300

    determine which tab is clicked

--- javascripts/app.js
+++ javascripts/app.js
@@ -4,9 +4,20 @@ var main = function () {
   $(".tabs a span").toArray().forEach(function (element){
     // create a click handler for the event
     $(element).on("click", function (){
+      // create a temporary jQuery version variable
+      var $element = $(element);
+
       $(".tabs a span").removeClass("active");
-      $(element).addClass("active");
+      $element.addClass("active");
       $("main .content").empty();
+
+      if ($element.parent().is(":nth-child(1)")) {
+        console.log("FIRST TAB IS CLICKED!");
+      }else if ($element.parent().is(":nth-child(2)")) {
+        console.log("SECOND TAB IS CLICKED");
+      }else if ($element.parent().is(":nth-child(3)")) {
+        console.log("THIRD TAB IS CLICKED");
+      }
       return false;
     })
   });
